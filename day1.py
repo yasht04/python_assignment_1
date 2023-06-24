3863e33a2a5de78cf5d1c9e65715d05dd3ebb06e
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2023-06-24 22:35:11 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,11 @@
+""" define any 3 variables with given marks or
+user-defined marks and calculate average.
+display the average in following format
+The Average of n1,n2 and n3 is __ """
+
+n1 = int(input("Enter first Value: "))
+n2 = int(input("Enter second Value: "))
+n3 = int(input("Enter third value: "))
+avg = (n1+n2+n3)/ 3
+
+print("The Average of",n1,n2,"and",n3,"is: ",avg)
